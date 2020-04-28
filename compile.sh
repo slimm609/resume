182df22a43a3841fb8f3a82531268fd100cad5d8
Brian Davis <brian.davis@ciphertrace.com>
2020-04-28 11:08:32 -0400

    update resume format and generate multiple styles and colors

--- compile.sh
+++ compile.sh
@@ -1,3 +1,22 @@
 #!/usr/bin/env bash
 
-docker run -v $(pwd):/latex --rm -it briandavis/latex pdflatex pdflatex resume.tex
+rm -rf output || true
+
+typeList=('casual' 'oldstyle' 'banking' 'classic')
+colorList=('blue' 'green' 'grey' 'orange' 'purple' 'red')
+
+for type in ${typeList[@]}; do
+  mkdir -p output/${type}
+  sed "s/TYPE/$type/" resume.tex > render-${type}.tex
+  for color in ${colorList[@]}; do
+    sed "s/COLOR/$color/" render-${type}.tex > render-${type}-${color}.tex
+    pdflatex render-${type}-${color}.tex
+    mv render-${type}-${color}.pdf output/${type}/
+  done
+done
+
+rm render*
+
+
+
+
